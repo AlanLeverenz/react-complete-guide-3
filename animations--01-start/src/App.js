d4233c0c5828c7ff34ba0cf35221a0ef3271969f
Alan Leverenz <awleverenz@aol.com>
2022-09-15 21:37:41 -0400

    added Transition to toggle button

--- animations--01-start/src/App.js
+++ animations--01-start/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from 'react-transition-group/Transition';
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -31,17 +32,9 @@ class App extends Component {
           Toggle
         </button>
         <br />
-        {this.state.showBlock ? (
-          <div
-            style={{
-              backgroundColor: 'red',
-              width: 100,
-              height: 100,
-              margin: 'auto'
-            }}>
-          </div>
-        ) : null
-        };
+        <Transition in={this.state.showBlock} timeout={300}>
+          {state => <p>{state}</p>}
+        </Transition>
         {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> : null}
         {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
