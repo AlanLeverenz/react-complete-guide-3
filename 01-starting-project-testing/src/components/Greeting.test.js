a64318fb0c92649e7c9f988654f14f39f0f99bbe
Alan Leverenz <awleverenz@aol.com>
2022-10-21 22:19:00 -0400

    setup group test

--- 01-starting-project-testing/src/components/Greeting.test.js
+++ 01-starting-project-testing/src/components/Greeting.test.js
@@ -1,18 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
-test('renders Hello World as a text', () => {
-  //  Arrange
-  render(<Greeting />);
+// setup test groups with describe function
+describe('Greeting component', () => {
+  test('renders Hello World as a text', () => {
+    //  Arrange
+    render(<Greeting />);
 
-  // Act
-  // ... nothing
+    // Act
+    // ... nothing
 
-  // Assert
-  const helloWorldElement = screen.getByText('Hello World', { exact: false });
-  expect(helloWorldElement).toBeInTheDocument();
+    // Assert
+    const helloWorldElement = screen.getByText('Hello World', { exact: false });
+    expect(helloWorldElement).toBeInTheDocument();
 
+  });
 });
+
+
 
 // Arrange - set up the test data, test conditions, and test environment
 // Act - run logic that should be tested (e.g. execute function)
