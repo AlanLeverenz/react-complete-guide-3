8c6894f0f571562392b9e8f7e64efa052fd9b57c
Alan Leverenz <awleverenz@aol.com>
2022-10-23 23:07:20 -0400

    added test for button click

--- 01-starting-project-testing/src/components/Greeting.test.js
+++ 01-starting-project-testing/src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greeting';
 
 // setup test groups with describe function
@@ -28,6 +29,21 @@ describe('Greeting component', () => {
     expect(outputElement).toBeInTheDocument();
 
   });
+
+  test('renders "Changed!" if the button was clicked', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement)
+
+    // Assert
+    const outputElement = screen.getByText('Changed!');
+    expect(outputElement).toBeInTheDocument();
+
+  });
+
 });
 
 
