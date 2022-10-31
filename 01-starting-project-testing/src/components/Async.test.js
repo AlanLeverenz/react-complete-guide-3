6c82423df8688ce401076ad0c2bb4042ca286a98
Alan Leverenz <awleverenz@aol.com>
2022-10-30 21:00:15 -0400

    added typescript project

--- 01-starting-project-testing/src/components/Async.test.js
+++ 01-starting-project-testing/src/components/Async.test.js
@@ -4,6 +4,12 @@ import Async from './Async';
 describe('Async component', () => {
   // Arrange
   test('renders posts if request succeeds', async () => {
+    // creates a mock function so there is no interaction with a server
+    // overwrites the fetch functionn with a built-in function
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 'p1', title: 'First post' }]
+    });
     render(<Async />)
 
     // Act...
