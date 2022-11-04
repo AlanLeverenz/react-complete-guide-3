741e7fd0d7260b7c7a5ad37e286d70c5ab527401
Alan Leverenz <awleverenz@aol.com>
2022-11-03 22:49:26 -0400

    generics

--- 01-starting-project-typescript/basics.ts
+++ 01-starting-project-typescript/basics.ts
@@ -48,3 +48,26 @@ let course: string | number = 'React - The Complete Guide';
 course = 1234;
 
 // Type alias
+
+// Functions & types
+// inferred return value type
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+// void is inferred for functions
+function printOuput(value: any) {
+  console.log(value);
+}
+
+// Generics
+// looks at the type of values to match with return type
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+// now it knows that this array is a number type
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAtBeginning(demoArray, -1);
+const stringArray = insertAtBeginning(['a', 'b', 'c', 'd'], 'd');
