57503f5e5a3d2a677b56e91be21c4479d4a762e9
Alan Leverenz <awleverenz@aol.com>
2022-11-07 19:45:21 -0500

    added comment

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -3,6 +3,7 @@ import Todo from './models/todo';
 
 function App() {
   const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')];
+  // uses todos as a prop to use in Todos
 
   return (
     <div>
