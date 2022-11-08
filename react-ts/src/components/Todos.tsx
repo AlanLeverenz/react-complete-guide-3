d674578d378543bc3bfae76d91273b8fdebd1412
Alan Leverenz <awleverenz@aol.com>
2022-11-07 19:42:10 -0500

    used a class component's objects for mapping

--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
+import Todo from '../models/todo';
+
 // @types/react
 // FC generic type
 // COMMAND CLICK on FC
 // using <> unlocks a feature that merges with children prop
 
-const Todos: React.FC<{ items: string[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item}>{item}</li>
+        <li key={item.id}>{item.text}</li>
       ))}
     </ul>
   );
---
+++ react-ts/src/models/todo.ts
@@ -0,0 +1,12 @@
+class Todo {
+  // need to define the properties ahead of the constructor
+  id: string;
+  text: string;
+
+  constructor(todoText: string) {
+    this.text = todoText;
+    this.id = new Date().toISOString();
+  }
+}
+
+export default Todo;
