685a8fbf1c10250d69755209f6900998b7176ecd
Alan Leverenz <awleverenz@aol.com>
2022-11-07 19:27:19 -0500

    added ReactFC parameter to function

--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -1,10 +1,18 @@
-function Todos() {
+import React from 'react';
+
+// @types/react
+// FC generic type
+// COMMAND CLICK on FC
+// using <> unlocks a feature that merges with children prop
+
+const Todos: React.FC<{ items: string[] }> = (props) => {
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Learn TypeScript</li>
+      {props.items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
     </ul>
   );
-}
+};
 
 export default Todos;
