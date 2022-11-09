00538f1484d34240cf4c4c6980fe4f068eeecd2b
Alan Leverenz <awleverenz@aol.com>
2022-11-08 21:18:14 -0500

    updated Todos

--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-
+import TodoItem from './TodoItem';
 import Todo from '../models/todo';
 
-// @types/react
-// FC generic type
 // COMMAND CLICK on FC
 // using <> unlocks a feature that merges with children prop
 
@@ -11,7 +9,7 @@ const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
