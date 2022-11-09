4b19579a9b3dddcdb3e2366b2335c52ddc08874f
Alan Leverenz <awleverenz@aol.com>
2022-11-09 11:30:31 -0500

    corrected entered code

--- react-ts/src/components/NewTodo.tsx
+++ react-ts/src/components/NewTodo.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { useRef } from 'react';
 
 const NewTodo = () => {
+  // set the concrete type for useRef
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    // ? was added because the ref is not yet set to a value yet
+    // ? signals to TS that it tries to get the value,
+    // otherwise null will be stored
+    // ! means the value will never be null
+    const enteredText = todoTextInputRef.current!.value;
   };
 
   // JSX code
@@ -10,7 +20,7 @@ const NewTodo = () => {
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor='text'>Todo text</label>
-      <input type='text' id='text' />
+      <input type='text' id='text' ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
   );
