aeccfadbceef11a64ddfc934f833a1f29413ce58
Alan Leverenz <awleverenz@aol.com>
2022-11-11 21:09:14 -0500

    added comments

--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -6,8 +6,9 @@ import classes from './Todos.module.css';
 // COMMAND CLICK on FC
 // using <> unlocks a feature that merges with children prop
 // items is a Todo array; onRemoveTodo is a function that does not return a value
-// use bind preconfigures a function for future execution
+// bind preconfigures a function for future execution
 // null is first object in the constructor (todo.ts)
+// onRemoveTodo is not returning a todo, only removing it (null)
 const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
   props
 ) => {
