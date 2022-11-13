79838fe5a22d8563a5ddf28bd4050571396df288
Alan Leverenz <awleverenz@aol.com>
2022-11-12 21:46:16 -0500

    updated App to wrap components in TodosContextProvider

--- react-ts/src/components/NewTodo.tsx
+++ react-ts/src/components/NewTodo.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import { useRef } from 'react';
+import React, { useRef, useContext } from 'react';
+
+import { TodosContext } from '../store/todos-context';
+
 import classes from './NewTodo.module.css';
 
 // () => describes the function and return type
 // void means there are no parameters
-const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+const NewTodo: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+
   // set the concrete type for useRef
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
 
-    // ? was added because the ref is not yet set to a value yet
+    // ? was added because the ref is not yet set to a value
     // ? signals to TS that it tries to get the value,
     // otherwise null will be stored
     // ! means the value will never be null
@@ -22,8 +26,8 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
       return;
     }
 
-    // store a function to use in App
-    props.onAddTodo(enteredText);
+    // using context
+    todosCtx.addTodo(enteredText);
   };
 
   // JSX code
