6601b8bb8588cfd11a021fe5b6b241afeeac4048
Alan Leverenz <awleverenz@aol.com>
2022-11-25 11:11:43 -0500

    fixed infinite loop by adding empty array dependency in useEffect function

--- hooks-05-send-http/src/components/Ingredients/Ingredients.js
+++ hooks-05-send-http/src/components/Ingredients/Ingredients.js
@@ -9,23 +9,23 @@ const Ingredients = () => {
 
   // useEffect runs after the component has run
   // it creates "side effects" 
-  useEffect();
-
-  fetch('https://react-hooks-update-66cf8-default-rtdb.firebaseio.com/ingredients.json')
-    .then(response => {
-      return response.json();
-    })
-    .then(responseData => {
-      const loadedIngredients = [];
-      for (const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount
-        });
-      }
-      // setUserIngredients(loadedIngredients);
-    });
+  useEffect(() => {
+    fetch('https://react-hooks-update-66cf8-default-rtdb.firebaseio.com/ingredients.json')
+      .then(response => {
+        return response.json();
+      })
+      .then(responseData => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
 
 
   const addIngredientHandler = ingredient => {
