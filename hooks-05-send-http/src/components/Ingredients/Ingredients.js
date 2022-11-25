43a5754e16a33fb165a94b6d0af4f2f89d4440bb
Alan Leverenz <awleverenz@aol.com>
2022-11-24 21:23:03 -0500

    added fetch to gather ingredients

--- hooks-05-send-http/src/components/Ingredients/Ingredients.js
+++ hooks-05-send-http/src/components/Ingredients/Ingredients.js
@@ -7,7 +7,25 @@ import Search from './Search';
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
+  // useEffect runs after the component has run
+  // it creates "side effects" 
   useEffect();
+
+  fetch('https://react-hooks-update-66cf8-default-rtdb.firebaseio.com/ingredients.json')
+    .then(response => {
+      return response.json();
+    })
+    .then(responseData => {
+      const loadedIngredients = [];
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        });
+      }
+    });
+
 
   const addIngredientHandler = ingredient => {
     fetch('https://react-hooks-update-66cf8-default-rtdb.firebaseio.com/ingredients.json', {
