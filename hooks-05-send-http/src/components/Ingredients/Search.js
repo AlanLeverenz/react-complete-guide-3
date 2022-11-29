eb399d3fcd3c2d897b233d9591ec2da1909a78ce
Alan Leverenz <awleverenz@aol.com>
2022-11-28 20:31:55 -0500

    inserted setTimeout to fetch when typing has paused

--- hooks-05-send-http/src/components/Ingredients/Search.js
+++ hooks-05-send-http/src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,26 +6,35 @@ import './Search.css';
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState('');
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query =
-      enteredFilter.length === 0
-        ? ''
-        : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch('https://react-hooks-update-66cf8-default-rtdb.firebaseio.com/ingredients.json' + query)
-      .then(response => response.json())
-      .then(responseData => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount
+    setTimeout(() => {
+      // check if the enteredFilter is the same after 500 ms
+      // enteredFilter is locked in when the time is set - the 'old' value
+      // compares 'old' value with current value which is not locked in since
+      // it was defined outside the useEffect closure
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ''
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch('https://react-hooks-update-66cf8-default-rtdb.firebaseio.com/ingredients.json' + query)
+          .then(response => response.json())
+          .then(responseData => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount
+              });
+            }
+            onLoadIngredients(loadedIngredients);
           });
-        }
-        onLoadIngredients(loadedIngredients);
-      });
-  }, [enteredFilter, onLoadIngredients]);
+      } // end if
+    }, 500) // end setTimeout
+  }, [enteredFilter, onLoadIngredients, inputRef]); // end useEffect
 
   return (
     <section className="search">
@@ -33,6 +42,7 @@ const Search = React.memo(props => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             value={enteredFilter}
             onChange={event => setEnteredFilter(event.target.value)}
